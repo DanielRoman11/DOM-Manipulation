8931cabe11fad940c8ba4911a63b0cffecdac800
danielroman11 <danielroman.05111@gmail.com>
2023-05-05 00:50:44 -0500

    Correcion Contador

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function crearCajon() {
   const span = document.getElementById("contador");
   span.innerText = contador
   let html = `<div class="caja" id="tarjeta${contador++}">
-  <h3>Caja# ${contador}</h3>
+  <h3>Caja# ${contador + 1}</h3>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, fugiat.</p>
 </div>`;
   desc.insertAdjacentHTML("afterend", html);
