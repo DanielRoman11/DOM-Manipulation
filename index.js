3061736ee31a026dd0c37241c5fdd6be5f90b214
danielroman11 <danielroman.05111@gmail.com>
2023-05-05 00:51:46 -0500

    contador

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let contador = 0;
+let contador = 1;
 function crearCajon() {
   const desc = document.getElementById("desc");
   const span = document.getElementById("contador");
   span.innerText = contador
   let html = `<div class="caja" id="tarjeta${contador++}">
-  <h3>Caja# ${contador + 1}</h3>
+  <h3>Caja# ${contador}</h3>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, fugiat.</p>
 </div>`;
   desc.insertAdjacentHTML("afterend", html);
