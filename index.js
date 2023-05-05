2358918d53ae30bc7758a941e7d6856cdfafbf54
danielroman11 <danielroman.05111@gmail.com>
2023-05-05 02:06:10 -0500

    Insertar Titulo, descripcion y validación del form

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ function crearCajon() {
   const desc = document.getElementById("desc");
   const span = document.getElementById("contador");
   span.innerText = contador
-  let html = `<div class="caja" id="tarjeta${contador++}">
-  <h3>Caja# ${contador}</h3>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, fugiat.</p>
-</div>`;
+
+  let titulo = document.getElementById("titulo");
+  let descripcion = document.getElementById("descripcion");
+
+  if( titulo.value == "" || descripcion.value == ""){
+    const boxError = document.getElementById("box-error");
+    return boxError.innerHTML = `<div class="error" id="error">
+    <p>Todos los campos son obligatorios</p>
+  </div>`
+  }
+  let error = document.getElementById("error");
+  if(error){ error.remove() }
+  let html = `<div class="caja" id="tarjeta${contador + 1}">
+  <h3>${titulo.value} -> #${contador}</h3>
+  <p>${descripcion.value}</p>
+  </div>`;
+  titulo.value = ""
+  descripcion.value = ""
+
   desc.insertAdjacentHTML("afterend", html);
+  contador++;
 }
 
 
