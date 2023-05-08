8a0c3dfc035ec30e6063aa34aa21d43bb8859947
danielroman11 <danielroman.05111@gmail.com>
2023-05-08 17:08:58 -0500

    Inserción en localstorage

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let id = 1;
+let span = document.getElementById("contador");
+const notas = []
+
 function crearCajon() {
-  const desc = document.getElementById("desc");
-  const span = document.getElementById("contador");
 
   let titulo = document.getElementById("titulo");
   let descripcion = document.getElementById("descripcion");
 
+  //* Verificar los campos y motrar errores
   if( titulo.value == "" || descripcion.value == ""){
     const boxError = document.getElementById("box-error");
     return boxError.innerHTML = `<div class="error" id="error">
@@ -15,22 +17,60 @@ function crearCajon() {
   let error = document.getElementById("error");
   if(error){ error.remove() }
 
-  let html = `<div class="caja" id="tarjeta${id}">
-  <h4 class="id">${id}</h4>
-  <h3>${titulo.value}</h3>
-  <hr />
-  <p>${descripcion.value}</p>
-  <div class="contenedor-icono">
-    <a class="texto" id="eliminar">Borrar</a>
-    <a class="texto" id="editar">Editar </a>
-  </div>
-  </div>`;
+  //* Insertar en localstorage
+  const nota = {
+    id: id,
+    titulo: titulo.value,
+    descripcion: descripcion.value
+  }
+  notas.push(nota)
+  console.log(notas)
+
+  localStorage.setItem("notas", JSON.stringify(notas));
+
   titulo.value = ""
   descripcion.value = ""
+  
+  for(let nota in storage) {
+    const desc = document.getElementById("desc");
+    let html = `
+    <div class="caja" id="tarjeta${storage[nota].id}">
+      <h4 class="id">${storage[nota].id}</h4>
+      <h3>${storage[nota].titulo}</h3>
+      <hr />
+      <p>${storage[nota].descripcion}</p>
+      <div class="contenedor-icono">
+        <a class="texto" id="eliminar">Borrar</a>
+        <a class="texto" id="editar">Editar </a>
+      </div>
+    </div>`;
+  
+    desc.insertAdjacentHTML("afterend", html);
+  }
   span.innerText = id
-
-  desc.insertAdjacentHTML("afterend", html);
-  return id++;
+  id++
 }
 
+let storage = JSON.parse(localStorage.getItem("notas"));
+console.log(storage);
 
+if(!storage){ span.innerText = "Lista vacia" } 
+else {
+  for(let nota in storage) {
+    const desc = document.getElementById("desc");
+    let html = `
+    <div class="caja" id="tarjeta${storage[nota].id}">
+      <h4 class="id">${storage[nota].id}</h4>
+      <h3>${storage[nota].titulo}</h3>
+      <hr />
+      <p>${storage[nota].descripcion}</p>
+      <div class="contenedor-icono">
+        <a class="texto" id="eliminar">Borrar</a>
+        <a class="texto" id="editar">Editar </a>
+      </div>
+    </div>`;
+  
+    desc.insertAdjacentHTML("afterend", html);
+  }
+  span.innerText = id
+}
