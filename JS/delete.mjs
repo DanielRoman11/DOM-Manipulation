d4530d62d0176ee647f6f277bb4876e958fb86e3
danielroman11 <danielroman.05111@gmail.com>
2023-05-17 23:54:47 -0500

    delete progress

--- JS/delete.mjs
+++ JS/delete.mjs
@@ -1,22 +1,29 @@
 import { getData } from './index.mjs';
 
 async function removerNota() {
-  let datos = await JSON.parse(localStorage.getItem("notas"));
-  //* Copiar los datos
-  const notas = [...datos];
-  console.log(notas);
   //* Iterar cada elemento de la tarjeta
-  document.querySelectorAll(".caja").forEach(el => {
+  let datos = await JSON.parse(localStorage.getItem("notas"));
+  
+  //* Copiar los datos
+  let notas = [...datos];
+  document.querySelectorAll(".caja").forEach(async el => {
     //* Evento de click en cada elemento
     el.addEventListener("click", e => {
-      let type = e.target.nodeName 
-      if(type == "A"){ //? Evento en el botón
+      let opcion = e.target.innerText
+      if(opcion == "Delete"){ //? Evento en el botón
         let targetId = e.target.parentNode.parentNode.id //? Obtener ID de la tarjeta en espécífico
         document.getElementById(targetId).remove()
+        
+        let id = e.target.parentNode.parentNode.firstElementChild.firstElementChild.innerText
+        notas.map(i => i.id == id ? notas.splice(i.id - 1, 1) : console.log("Not deleted"))
+        console.log(notas);
+
+        localStorage.setItem("notas", JSON.stringify(notas))
       }
     });
   });
-  let contador = document.getElementById("contador")
+  //? Cambiando el contador
+  document.getElementById("contador").innerText = Object.keys(datos).length;
 }
 
 export default removerNota;
